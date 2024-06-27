6d7d9abe2b475db3b591961b10a9d8bcd6d76e2a
tkarasov <tkarasov@gmail.com>
2024-06-27 17:26:00 -0600

    p0 calculations
    working on including p0 calculations

--- scripts/fitness_exp/combine_genotype_tailocin_2024.R
+++ scripts/fitness_exp/combine_genotype_tailocin_2024.R
@@ -31,10 +31,15 @@ dds$plant <- factor(dds$plant, levels = c("ctrl","col_0", "ey15_2"))
 #perform the median of ratios method of normalization
 dds <- estimateSizeFactors(dds)
 dds <- DESeq(dds)
+mat <- fpm(dds, robust = FALSE)
 resultsNames(dds) # lists the coefficients
 res <- results(dds, name="plant_ey15_2_vs_ctrl" )
 res_eyach <- res[,c("baseMean", "log2FoldChange", "pvalue", "padj" )]
 colnames(res_eyach) <- c("baseMean_Ey", "log2FoldChange_Ey", "pvalue_Ey", "padj_Ey" )
+
+# need to add a column that is the initial frequency of the gene
+p0_eyach_t0_p25c2<- dds[,which(dds$plant=="ctrl" & dds$treatment=="p25c2")]
+res_eyach$p0_axenic <- rowMeans(fpm(p0_eyach_t0_p25c2, robust = FALSE))/1000000
 
 #Now we want to convert Talia B's tnseq result file to the general ortholog mappings
 ortho <- read.table("/Users/talia/Documents/GitHub/TnSeq_Pseudomonas_Genotype/input_data/orthologues_dc3000_p25_c2_7_2022.txt", header = T, sep= "\t")
@@ -42,13 +47,13 @@ counts_table = p25_22_counts
 ortho_dict <- ortho[,3]
 names(ortho_dict) <- ortho[,1]
 reverse_dict_BJ_to_WP_DAK <- names(ortho_dict)
-names(reverse_dict) <- ortho_dict
+names(reverse_dict_BJ_to_WP_DAK) <- ortho_dict
 
 #add the BJ to DAK mapping. I need an ortho file with the DAKCFMEO correspondence
 
 #in the meantime we will just look at ones that have ortholog in DC3000
 BJ_val <- tb_p25$ID
-conversion <- reverse_dict[BJ_val]
+conversion <- reverse_dict_BJ_to_WP_DAK[BJ_val]
 tb_p25$DC_ID <- conversion
 tb_converted <- tb_p25 %>% filter(conversion!="NA")
 #rownames(tb_converted) <- tb_converted$DC_ID
